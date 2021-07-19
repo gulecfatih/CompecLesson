47e360ebc1da6bc1c942515c2c27f68461c8807d
gulecfatih <56516242+gulecfatih@users.noreply.github.com>
2021-07-20 01:10:32 +0300

    Refactor: First Lesson Scripts

--- Assets/Scripts/LessonsBasic/FirstLessonScript.cs
+++ Assets/Scripts/LessonsBasic/FirstLessonScript.cs
@@ -13,12 +13,12 @@ namespace LessonsBasic
         // Start is called before the first frame update
 
 
-        public int tamSayi = 1;
-        [SerializeField] int tamSayi2 = 100;
-        float ondalikSayi = 0.25f;
-        float ondalikSayi2 = 5.2342f;
-        bool dogru = true;
-        bool Yanlis2 = false;
+        //public int tamSayi = 1;
+        //[SerializeField] int tamSayi2 = 100;
+        //float ondalikSayi = 0.25f;
+        //float ondalikSayi2 = 5.2342f;
+        //bool dogru = true;
+        //bool Yanlis2 = false;
         void Start()
         {
 
