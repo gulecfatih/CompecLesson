a9e8f9ee7bf07cb830c7209fa8c513e2eda7b756
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-08-27 12:55:28 +0300

    +refactor: PlayerMovementController and LocalPlayer

--- Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
+++ Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
@@ -14,11 +14,10 @@ namespace TopDownShooter.PlayerMovement
 
         private void Update()
         {
-            _rigidbody.MovePosition(_rigidbody.position+ (_rigidbody.transform.forward* _inputData.Vertical 
-                * _playerMovementSettings.VerticalSpeed)); 
-            _rigidbody.MovePosition(_rigidbody.position +(_rigidbody.transform.right * _inputData.Horizontal
-                *_playerMovementSettings.HorizontalSpeed));
+            _rigidbody.MovePosition(_rigidbody.position+ (_rigidbody.transform.forward*
+                _inputData.Vertical* _playerMovementSettings.VerticalSpeed)); 
+            _rigidbody.MovePosition(_rigidbody.position +(_rigidbody.transform.right
+                * _inputData.Horizontal *_playerMovementSettings.HorizontalSpeed));
         }
     }
 }
-
