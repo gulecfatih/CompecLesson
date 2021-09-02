b019671da322ab89cbc15ca37ab6bd85b4835997
DESKTOP-POINQD4\gulec <gulecfatih54@gmail.com>
2021-09-02 13:51:42 +0300

    +refactor Input system

--- Assets/TopDownShooter/Scripts/Input/InputData.cs
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,70 @@ namespace TopDownShooter.PlayerInput
         public float Horizontal;
         public float Vertical;
 
+        [Header("Axis Base Control")]
+        [SerializeField] private bool _axisActive;
+        [SerializeField] private string AxisNameHorizontal;
+        [SerializeField] private string AxisNameVertical;
+
+
+        [Header("Key Base Control")]
+
+
+       
+
+        [SerializeField] private bool _keyBaseHorizontalActive;
+        [SerializeField] private KeyCode PositiveHorizontalKeyCode;
+        [SerializeField] private KeyCode NegativeHorizontalKeyCode;
+        [SerializeField] private bool _keyBaseVerticalActive;
+        [SerializeField] private KeyCode PositiveVerticalKeyCode;
+        [SerializeField] private KeyCode NegativeVerticalKeyCode;
+        [SerializeField] private float increaseAmount = 0.015f;
+
+
+
+        public void ProcessInput()
+        {
+            if (_axisActive)
+            {
+                Horizontal = Input.GetAxis(AxisNameHorizontal);
+                Vertical = Input.GetAxis(AxisNameVertical);
+
+            }
+            else
+            {
+                if (_keyBaseHorizontalActive)
+                {
+                    KeyBaseHorizontal(ref Horizontal, PositiveHorizontalKeyCode, NegativeHorizontalKeyCode);
+                }
+                if (_keyBaseVerticalActive)
+                {
+                    KeyBaseHorizontal(ref Vertical, PositiveVerticalKeyCode, NegativeVerticalKeyCode);
+                }
+            }
+        }
+
+        private void KeyBaseHorizontal(ref float value, KeyCode positive, KeyCode negative)
+        {
+            bool positeveActive = Input.GetKey(positive);
+            bool negativeActive = Input.GetKey(negative);
+
+            if (positeveActive )
+            {
+                value += increaseAmount;
+            }
+            else if (negativeActive)
+            {
+                value -= increaseAmount;
+            }
+            else
+            {
+                value = 0;
+            }
+            value = Mathf.Clamp(value, -1, 1);
+        }
+
     }
+
+
 
 }
